51838f2aaa15ca127fe1bf9eca5ebaf8440997a9
steve kaci <kaci.steve@k-net.fr>
2021-11-08 16:06:04 +0100

    version000.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       </header>
       <section>
           <h1>Contenu</h1>
+          <button type="button" className="btn btn-primary">primary</button>
       </section>
       <footer>
           <h2>Footer</h2>
