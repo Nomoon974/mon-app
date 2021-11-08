d14f5209fb1e284bce97b0583fddf422bf0d819d
steve kaci <kaci.steve@k-net.fr>
2021-11-08 16:21:02 +0100

    version 000.3

--- src/App.js
+++ src/App.js
@@ -7,17 +7,38 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width= "50" />
-        <h2>Header</h2>
-        
-        
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src={logo} className="App-logo" alt="logo" width="50" />
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Gallery</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+
       </header>
-      <section>
-          <h1>Contenu</h1>
-          <button type="button" className="btn btn-primary">primary</button>
+      <section className="text-center">
+        <h1>Contenu</h1>
+        <button type="button" className="btn btn-primary">primary</button>
       </section>
-      <footer>
-          <h2>Footer</h2>
+      <footer className="bg-dark text-white text-center p-2">
+        <h2>© CCI DWWM</h2>
       </footer>
     </div>
   );
