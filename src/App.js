d6c1bf99cc9e646b204b22ea03bdf6513e3a86b5
Lucas <luky974@gmail.com>
2021-11-08 16:58:36 +0100

    version 000.4

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import Footer from './component/Footer';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src={logo} className="App-logo" alt="logo" width="50" />
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Gallery</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-
-      </header>
-      <section className="text-center">
-        <h1>Contenu</h1>
-        <button type="button" className="btn btn-primary">primary</button>
+      <Header />
+      <section className="text-center bg-danger">
+        <h1 className="txt-danger">Contenu</h1>
+        <button type="button" className="btn btn-primary m-1">primary</button>
       </section>
-      <footer className="bg-dark text-white text-center p-2">
-        <h2>© CCI DWWM</h2>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return( <footer className="bg-dark text-white text-center p-2">
+    <h2>© CCI DWWM</h2>
+  </footer>
+  )
+}
+export default Footer
